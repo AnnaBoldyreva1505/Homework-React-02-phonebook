7ee8594b64a4973b88c51598484b39b5843505d3
AnnaBoldyreva1505 <hannabl1988@gmail.com>
2023-05-11 21:37:49 +0600

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,66 @@
-export const App = () => {
-  return (
-    <>
-      React homework template
-    </>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+    number: '',
+  };
+
+  render() {
+    return (
+      <>
+        {/* <h1>Phonebook</h1>
+  <ContactForm ... />
+
+  <h2>Contacts</h2>
+  <Filter ... />
+  <ContactList ... /> */}
+
+        <h1>Phone Book</h1>
+        <label>Name</label>
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+        <label>Phone</label>
+        <input
+          type="tel"
+          name="number"
+          placeholder="Enter phone number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+
+        <button type="button">Add contact</button>
+
+        <h2>Contacts</h2>
+        <p>Find contacts by name</p>
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+        <ul>
+          <li>
+            Имя<button>Удалить</button>
+          </li>
+        </ul>
+      </>
+    );
+  }
+}
